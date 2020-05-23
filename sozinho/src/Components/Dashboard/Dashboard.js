84080e7b5e3340f7b102b1f3f742b16b907b443e
JoaoPw <dev_joaopedro@outlook.com>
2020-05-23 16:45:46 -0300

    Inicial things

---
+++ sozinho/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Dashboard.scss'
+
+const dashboard = () => {
+    return (
+        <div className="Dashboard">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Suspendisse imperdiet a odio eget congue. Ut commodo nisi quam, 
+            in posuere felis fermentum at. Aenean quis massa rutrum, vulputate 
+            nibh eu, feugiat orci. Maecenas vitae ligula ac orci tincidunt iaculis. 
+            Etiam iaculis turpis sem, a dictum magna semper et. Curabitur accumsan 
+            tellus et congue volutpat. Proin efficitur vitae quam scelerisque porta. 
+            Sed gravida, turpis non ultrices posuere, nibh felis scelerisque felis,
+            id bibendum dolor ipsum sit amet arcu. Vivamus dictum accumsan efficitur.
+            Vivamus suscipit vitae felis sit amet efficitur. Pellentesque at mollis 
+            metus, vitae luctus elit. Aenean non ornare lorem. Vestibulum in purus lorem
+            </p>
+        </div>
+    )
+}
+
+export default dashboard
---
+++ sozinho/src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.scss'
+
+const headerNav = () => {
+    return (
+        <header className="HeaderNav">
+            <a href="#">Purpose</a>
+        </header>
+    )
+}
+
+export default headerNav
